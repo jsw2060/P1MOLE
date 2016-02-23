e9132f1eed8faec568785c0b806c8efb07cc8db7
shwoo9807 <shwoo9807@naver.com>
2016-02-24 08:09:01 +0900

    오타수정

--- P1MOLE/src/com/client/ClientMainForm.java
+++ P1MOLE/src/com/client/ClientMainForm.java
@@ -131,7 +131,7 @@ public class ClientMainForm extends JFrame implements ActionListener, Runnable, 
 	public void connection(String id, String pwd, String sex) {
 		try {
 			//s = new Socket("211.238.142.85", 9469);
-			s = new Socket("127.0.0.1", 9469);
+			s = new Socket("211.238.142.85", 9469);
 			// s=>server
 			in = new BufferedReader(new InputStreamReader(s.getInputStream()));
 			out = s.getOutputStream();
